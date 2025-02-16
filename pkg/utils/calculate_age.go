3f50ae4fb4e262028d8dd1865d65650437121e61
erickxpto <egledsonos@gmail.com>
2025-02-16 19:13:11 -0300

    Update calculate_age.go
    Fix age calculator  function

--- pkg/utils/calculate_age.go
+++ pkg/utils/calculate_age.go
@@ -3,8 +3,7 @@ package utils
 import "time"
 
 func CalculateAge(birthday time.Time) int {
-	//now := time.Now()
-	now := time.Date(2025, time.February, 13, 0, 0, 0, 0, time.UTC)
+	now := time.Now()
 	age := now.Year() - birthday.Year()
 
 	if now.YearDay() < birthday.YearDay() {
